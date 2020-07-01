02c183fc985a2b913242f1de90d71b8f4c40a494
Darc99 <lennyyhugoh@gmail.com>
2020-07-01 23:38:29 +0100

    outline the different sections

---
+++ src/components/Products/Products.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Products.css'
+
+class Products extends Component {
+
+  render() {
+
+    return (
+
+      <div className= 'products'>
+        Products
+      </div>
+
+    )
+  }
+}
+
+export default Products;
