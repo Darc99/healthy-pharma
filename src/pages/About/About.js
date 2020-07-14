2f6e506286b0bfbe4d853391cc779c5d88001173
Darc99 <lennyyhugoh@gmail.com>
2020-07-14 23:14:05 +0100

    added hero to home

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,18 +1,113 @@
-import React, { Component } from 'react';
-import './About.css'
+import React from 'react';
 
-class About extends Component {
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { Card, CardMedia, CardContent, CardActions, CardActionArea } from '@material-ui/core';
 
-  render() {
+const useStyles = makeStyles((theme) => ({
+  aboutSection: {
+    paddingTop: theme.spacing(3)
+  },
+  aboutTitle: {
+    //float:"left",
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3)
+  },
+  card: {
+    maxWidth: "100%"
+  },
+  media: {
+    height: 240
+  }
+}));
+
+function About() {
+    const classes = useStyles();
 
     return (
 
-      <div className= 'about'>
-        About
+      <div>
+
+        <Container maxWidth="lg" className={classes.aboutSection}>
+            <Typography variant="h4" className={classes.aboutTitle}>
+              About Us
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia 
+                      className={classes.media}
+                      image=""
+                      title=""
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Original
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                         Our drugs are made using only the finest of technologies
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+
+                  </CardActions>
+                </Card>
+              </Grid>              
+
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                    <CardActionArea>
+                      <CardMedia 
+                        className={classes.media}
+                        image=""
+                        title=""
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          Professional
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                        Our staff holds some of the most sort after professionals on the planet
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+
+                    </CardActions>
+                  </Card>
+              </Grid>              
+
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia 
+                      className={classes.media}
+                      image=""
+                      title=""
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Safe
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                      Our drugs are made with saftey in mind. We assure no side effects will ensue upon using drugs from us.
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+
+                  </CardActions>
+                </Card>
+              </Grid>
+            </Grid>
+        </Container>
+
       </div>
 
     )
-  }
 }
-
 export default About;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import classes from '*.module.css';
+import backg from '../../img/backg.jpg';
+import Box from '@material-ui/core/Box';
+
 
 const useStyles = makeStyles((theme) => ({
   hero: {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5) (rgba(0, 0, 0, 0.5)), url()`
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5) (rgba(0, 0, 0, 0.5)), url
+    (${backg})`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    fontSize: "4rem",
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      fontSize: "3em"
+    }
   }
 }));
 
 
 function Home() {
+    const classes = useStyles();
 
     return (
 
       <div>
         <Box className={classes.hero}>
-
+          <Box>Medic</Box>
         </Box>
       </div>
 
