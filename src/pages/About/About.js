b85bbf93269c3733ea119a8d07787ce5e75ac249
Darc99 <lennyyhugoh@gmail.com>
2020-07-15 01:07:27 +0100

    added mui to the service page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,8 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { Card, CardMedia, CardContent, CardActions, CardActionArea } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActionArea, Paper, Box, Button } from '@material-ui/core';
 
+// CardActions
 const useStyles = makeStyles((theme) => ({
   aboutSection: {
     paddingTop: theme.spacing(3)
@@ -20,6 +21,12 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 240
+  },
+  aboutBelow: {
+    paddingTop: theme.spacing(4)
+  },
+  paper: {
+    height: 360
   }
 }));
 
@@ -52,9 +59,7 @@ function About() {
                       </Typography>
                     </CardContent>
                   </CardActionArea>
-                  <CardActions>
-
-                  </CardActions>
+                  {/* <CardActions></CardActions> */}
                 </Card>
               </Grid>              
 
@@ -75,9 +80,7 @@ function About() {
                         </Typography>
                       </CardContent>
                     </CardActionArea>
-                    <CardActions>
-
-                    </CardActions>
+                    {/* <CardActions></CardActions> */}
                   </Card>
               </Grid>              
 
@@ -98,10 +101,32 @@ function About() {
                       </Typography>
                     </CardContent>
                   </CardActionArea>
-                  <CardActions>
-
-                  </CardActions>
+                  {/* <CardActions></CardActions> */}
                 </Card>
+              </Grid>          
+
+            </Grid>
+            
+            <Grid container className={classes.aboutBelow}>
+              <Grid item xs={12} sm="auto" md={6}>
+                <Paper className={classes.paper}></Paper>
+              </Grid>
+              <Grid item xs={12} sm="auto" md={6}>
+                <Paper className={classes.paper}>
+                <Typography variant="body2" color="textSecondary" component="p">
+                At Evanto Care we are strong belivers in the wealth that
+                  lies in health and as such we always seek for the
+                  perfection of health within our clients. With 60 years of
+                  service to humanity, we are constantly breaking barriers
+                  that prevent man from achiveing the perfect well
+                  being. Personalized drugs make it easier for illnesses to
+                  be managed and of course at pocket friendly prices.      
+                </Typography>            
+                  <Box>
+                    <Button variant="contained">Read More</Button>
+                  </Box>
+                </Paper>
+
               </Grid>
             </Grid>
         </Container>
