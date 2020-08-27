432a475d86da249a8f3947415672e12f908b418f
Darc99 <lennyyhugoh@gmail.com>
2020-08-27 23:22:21 +0100

    working on products

--- src/components/Drugs/Drugs.jsx
+++ src/components/Drugs/Drugs.jsx
@@ -1,29 +1,9 @@
 import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  serviceSection: {
-    paddingTop: theme.spacing(3)
-  },
-  serviceTitle: {
-    //float:"left",
-    fontWeight: 800,
-    paddingBottom: theme.spacing(3)
-  },
-  card: {
-    maxWidth: "100%"
-  },
-  media: {
-    height: 240
-  }
-}));
-
 
 class Drugs extends Component {
   
   render() {
     console.log(this.props.drugs)
-    const classes = useStyles();
     return (
       <div>
         {this.props.drugs.map((drug) => (
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { Card, CardMedia, CardContent, CardActionArea, Paper, Box, Button } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActionArea, Paper, Box, CardActions } from '@material-ui/core';
 const drugs = [
   {
     id: 1,
@@ -51,10 +51,10 @@ const drugs = [
 ]
 // CardActions
 const useStyles = makeStyles((theme) => ({
-  aboutSection: {
+  serviceSection: {
     paddingTop: theme.spacing(3)
   },
-  aboutTitle: {
+  serviceTitle: {
     //float:"left",
     fontWeight: 800,
     paddingBottom: theme.spacing(3)
@@ -64,12 +64,6 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 240
-  },
-  aboutBelow: {
-    paddingTop: theme.spacing(4)
-  },
-  paper: {
-    height: 360
   }
 }));
 
@@ -78,14 +72,14 @@ function Products() {
     return (
 
       <div>
-        
-        <Container maxWidth="lg" className={classes.aboutSection}>
-            <Typography variant="h4" className={classes.aboutTitle}>
+
+        <Container maxWidth="lg" className={classes.serviceSection}>
+            <Typography variant="h4" className={classes.serviceTitle}>
               Products
             </Typography>
-            <Drugs drugs={drugs}/>
+            <Drugs drugs={drugs} />
             <Grid container spacing={3}>
-              <Grid item xs={12} sm={6} md={4}>
+              <Grid item xs={12} sm={4} md={4}>
                 <Card className={classes.card}>
                   <CardActionArea>
                     <CardMedia 
@@ -93,20 +87,20 @@ function Products() {
                       image=""
                       title=""
                     />
-                    <CardContent>
+                    {/* <CardContent>
                       <Typography gutterBottom variant="h5" component="h2">
                         Original
                       </Typography>
                       <Typography variant="body2" color="textSecondary" component="p">
                          Our drugs are made using only the finest of technologies
                       </Typography>
-                    </CardContent>
+                    </CardContent> */}
                   </CardActionArea>
-                  {/* <CardActions></CardActions> */}
+                  <CardActions>Medical Checkups</CardActions>
                 </Card>
               </Grid>              
 
-              <Grid item xs={12} sm={6} md={4}>
+              <Grid item xs={12} sm={4} md={4}>
                 <Card className={classes.card}>
                     <CardActionArea>
                       <CardMedia 
@@ -114,20 +108,20 @@ function Products() {
                         image=""
                         title=""
                       />
-                      <CardContent>
+                      {/* <CardContent>
                         <Typography gutterBottom variant="h5" component="h2">
                           Professional
                         </Typography>
                         <Typography variant="body2" color="textSecondary" component="p">
                         Our staff holds some of the most sort after professionals on the planet
                         </Typography>
-                      </CardContent>
+                      </CardContent> */}
                     </CardActionArea>
-                    {/* <CardActions></CardActions> */}
+                    <CardActions>Laboratory Services</CardActions>
                   </Card>
               </Grid>              
 
-              <Grid item xs={12} sm={6} md={4}>
+              <Grid item xs={12} sm={4} md={4}>
                 <Card className={classes.card}>
                   <CardActionArea>
                     <CardMedia 
@@ -135,43 +129,20 @@ function Products() {
                       image=""
                       title=""
                     />
-                    <CardContent>
+                    {/* <CardContent>
                       <Typography gutterBottom variant="h5" component="h2">
                         Safe
                       </Typography>
                       <Typography variant="body2" color="textSecondary" component="p">
                       Our drugs are made with saftey in mind. We assure no side effects will ensue upon using drugs from us.
                       </Typography>
-                    </CardContent>
+                    </CardContent> */}
                   </CardActionArea>
-                  {/* <CardActions></CardActions> */}
+                  <CardActions>Medical Counselling</CardActions>
                 </Card>
               </Grid>          
 
-            </Grid>
-            
-            <Grid container className={classes.aboutBelow}>
-              <Grid item xs={12} sm="auto" md={6}>
-                <Paper className={classes.paper}></Paper>
-              </Grid>
-              <Grid item xs={12} sm="auto" md={6}>
-                <Paper className={classes.paper}>
-                <Typography variant="body2" color="textSecondary" component="p">
-                At Evanto Care we are strong belivers in the wealth that
-                  lies in health and as such we always seek for the
-                  perfection of health within our clients. With 60 years of
-                  service to humanity, we are constantly breaking barriers
-                  that prevent man from achiveing the perfect well
-                  being. Personalized drugs make it easier for illnesses to
-                  be managed and of course at pocket friendly prices.      
-                </Typography>            
-                  <Box>
-                    <Button variant="contained">Read More</Button>
-                  </Box>
-                </Paper>
-
-              </Grid>
-            </Grid>
+            </Grid>   
         </Container>
 
       </div>
