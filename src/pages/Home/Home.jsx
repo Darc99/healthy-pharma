0edd1d2c4f726cd41204e276f00d85633986b20a
Darc99 <lennyyhugoh@gmail.com>
2020-09-28 23:57:38 +0100

    home page img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import backg from '../../img/backg.jpg';
+import home from '../../img/home3.jpg';
 import Box from '@material-ui/core/Box';
 
 
@@ -11,6 +11,7 @@ const useStyles = makeStyles((theme) => ({
     // ('https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80')`,
     height: "500px",
     width: "100%",
+    // backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5) (rgba(0, 0, 0, 0.5))` ,
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
@@ -36,7 +37,7 @@ function Home() {
       <div>
         <Box className={classes.hero}>
           {/* <Box>YOOOO</Box> */}
-          <img src={backg}/>
+          <img src={home} className={classes.hero}/>
         </Box>
       </div>
 
